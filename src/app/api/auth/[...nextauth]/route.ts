5af606f5de34cf0042b7a4cbeaa17eee19a5a2b3
Fn-front <reservoir-station@outlook.jp>
2024-10-13 11:17:59 +0900

    add: lib nextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,7 @@
-import NextAuth from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
+import NextAuth from 'next-auth/next';
 
-export default NextAuth({
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-  // デフォルト設定（メールリンクの有効期限など）
-  session: {
-    strategy: 'jwt',
-  },
-  // その他のNextAuth設定
-});
+import options from '@/lib/nextAuth/options';
+
+const handler = NextAuth(options);
+
+export { handler as GET, handler as POST };
