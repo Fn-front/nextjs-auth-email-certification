939f9afe40bc71d6362a6ad742753f66b7b44273
Fn-front <reservoir-station@outlook.jp>
2024-10-13 15:27:39 +0900

    update: sessionごとに表示を変更

--- src/app/user/email/page.tsx
+++ src/app/user/email/page.tsx
@@ -5,11 +5,16 @@ import { useSession, signIn, signOut } from 'next-auth/react'
 /* eslint-disable */
 export default function SignIn() {
   const { data: session } = useSession()
-
   return (
     <div>
-      <h1>Sign In</h1>
-      <button onClick={() => signIn()}>Sign in</button>
+      {!!session && <>
+        <p>myName: {session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>}
+      {!session && <>
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
+      }
     </div>
   );
 }
