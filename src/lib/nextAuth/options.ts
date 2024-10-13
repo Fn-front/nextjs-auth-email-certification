72af9e74e7d864d062af323a65a8bbfc5bd11271
Fn-front <reservoir-station@outlook.jp>
2024-10-13 17:19:43 +0900

    update: port

--- src/lib/nextAuth/options.ts
+++ src/lib/nextAuth/options.ts
@@ -20,7 +20,7 @@ const options: NextAuthOptions = {
         const transporter = nodemailer.createTransport({
           host: process.env.EMAIL_SERVER_HOST,
           port: Number(process.env.EMAIL_SERVER_PORT),
-          secure: Number(process.env.EMAIL_SERVER_PORT) === 465, // SSL/TLS設定
+          secure: Number(process.env.EMAIL_SERVER_PORT) === 587, // SSL/TLS設定
           auth: {
             user: process.env.EMAIL_SERVER_USER,
             pass: process.env.EMAIL_SERVER_PASSWORD,
