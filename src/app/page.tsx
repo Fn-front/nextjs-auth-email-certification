f44f6c5107a30c961c0bfe0ebe98d2140c1c8b76
Fn-front <reservoir-station@outlook.jp>
2024-10-13 15:58:20 +0900

    update: mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useSession, signIn, signOut } from 'next-auth/react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 /* eslint-disable */
 export default function SignIn() {
@@ -8,11 +11,14 @@ export default function SignIn() {
   return (
     <div>
       {!!session && <>
-        <p>myName: {session.user?.email}</p>
-        <button onClick={() => signOut()}>Sign Out</button>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <p>myName:</p>
+          <TextField variant="outlined" defaultValue={session.user?.email} sx={{ input: { color: '#fff', width: '300px', fontSize: '1.5rem' }, fieldset: { borderColor: '#fff' }, paddingLeft: '16px' }} />
+        </Box>
+        <Button variant="outlined" size="large" onClick={() => signOut()} sx={{ fontSize: '1.5rem', marginTop: '16px' }}>Sign Out</Button>
       </>}
       {!session && <>
-        <button onClick={() => signIn()}>Sign in</button>
+        <Button variant="outlined" size="large" onClick={() => signIn()} sx={{ fontSize: '1.5rem' }}>Sign in</Button>
       </>
       }
     </div>
