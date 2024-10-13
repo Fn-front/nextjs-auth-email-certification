82f9683f6404bfc7ab5c55ff70f63216f076f71d
Fn-front <reservoir-station@outlook.jp>
2024-10-13 15:39:12 +0900

    fix: rootに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+'use client'
+
+import { useSession, signIn, signOut } from 'next-auth/react'
+
+/* eslint-disable */
+export default function SignIn() {
+  const { data: session } = useSession()
   return (
-    <>
-      <h2 className='c_h2'>テンプレ</h2>
-    </>
+    <div>
+      {!!session && <>
+        <p>myName: {session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>}
+      {!session && <>
+        <button onClick={() => signIn()}>Sign in</button>
+      </>
+      }
+    </div>
   );
 }
